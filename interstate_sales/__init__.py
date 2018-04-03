83a22ee42e7c170208243ca6da9fb45b55a325fd
Chris Closser <chrisclosser1@gmail.com>
2018-04-03 14:14:26 -0700

    deploy to heroku 15 init

--- interstate_sales/__init__.py
+++ interstate_sales/__init__.py
@@ -1,12 +1,15 @@
 from pyramid.config import Configurator
+import os
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    settings['sqlalchemy.url'] = os.environ.get('DATABASE_URL')
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
     config.include('.routes')
+    config.include('.views')
     config.scan()
     return config.make_wsgi_app()
