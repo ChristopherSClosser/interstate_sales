62cdde8487e790f2e4377b9a31b0387fd4262e2b
Chris Closser <chrisclosser1@gmail.com>
2018-04-03 17:58:22 -0700

    guardrail category JSON

--- interstate_sales/models/mymodel.py
+++ interstate_sales/models/mymodel.py
@@ -11,8 +11,12 @@ from .meta import Base
 class MyModel(Base):
     __tablename__ = 'models'
     id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+    category = Column(Text)
+    subcategory = Column(Text)
+    title = Column(Text)
+    img = Column(Text)
+    markdown = Column(Text)
+    extra = Column(Text)
 
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
