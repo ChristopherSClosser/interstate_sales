c0fe1706a4d45fa40f4e7eb5c1e7883d894924bb
Chris Closser <chrisclosser1@gmail.com>
2019-01-22 11:47:42 -0800

    add slide show jq - do not deploy -

--- interstate_sales/static/scripts/app.js
+++ interstate_sales/static/scripts/app.js
@@ -10,3 +10,26 @@ $(function(){
     }
   })
 });
+
+// displays gallery imgs in slide show format
+$(function () {
+  /* time and transition settings */
+  let change_img_time 	= 6000,
+    transition_speed	= 400,
+    simple_slideshow	= $('#slider'),
+    listItems	= simple_slideshow.children('li'),
+    listLen	= listItems.length, i	= 0,
+
+    changeList = function () {
+      listItems.eq(i).fadeOut(transition_speed, function () {
+        i += 1;
+        if (i === listLen) {
+          i = 0;
+        }
+        listItems.eq(i).fadeIn(transition_speed);
+      });
+    };
+
+  listItems.not(':first').hide();
+  setInterval(changeList, change_img_time);
+});
