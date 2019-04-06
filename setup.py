21abbe2e146b27182cc4ec8d41639c6341f2aa5a
Chris Closser <chrisclosser1@gmail.com>
2019-04-06 10:17:17 -0700

    version 0.0.2b -test deploy

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ tests_require = [
 
 setup(
     name='interstate_sales',
-    version='0..0.2b',
+    version='0.0.2b',
     description='interstate_sales',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
